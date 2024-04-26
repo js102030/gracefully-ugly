e3bd0afb1d5e43be0609d27abddae57616756103
js102030 <121748946+js102030@users.noreply.github.com>
2024-04-26 20:05:48 +0900

    Add files via upload

---
+++ backend/gracefully-ugly/src/main/java/com/gracefullyugly/GracefullyUglyApplication.java
@@ -0,0 +1,13 @@
+package com.gracefullyugly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GracefullyUglyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GracefullyUglyApplication.class, args);
+	}
+
+}
---
+++ backend/gracefully-ugly/src/test/java/com/gracefullyugly/GracefullyUglyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracefullyugly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracefullyUglyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
