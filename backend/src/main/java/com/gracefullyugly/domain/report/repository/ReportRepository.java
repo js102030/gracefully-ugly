0394de8e27b7575a6b468fe931c9a36937cee86b
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-04-29 13:48:32 +0900

    feat : report 기본 패키지만 만들어둔 상태입니다.

---
+++ backend/src/main/java/com/gracefullyugly/domain/report/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.gracefullyugly.domain.report.repository;
+
+import com.gracefullyugly.domain.report.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
