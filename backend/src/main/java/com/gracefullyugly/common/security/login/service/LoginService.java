65dcdbdc9dd236f02a967d60e112bbb52f079a16
SeokhyunRew <darkknightttt@naver.com>
2024-04-29 22:59:25 +0900

    feat : 로그인 서비스 로직 생성

---
+++ backend/src/main/java/com/gracefullyugly/common/security/login/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.gracefullyugly.common.security.login.service;
+
+import com.gracefullyugly.domain.user.entity.User;
+import com.gracefullyugly.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String longId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(longId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 아이디가 존재하지 않습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getLoginId())
+                .password(user.getPassword())
+                .roles("ROLE_" + user.getRole().name())
+                .build();
+    }
+}
