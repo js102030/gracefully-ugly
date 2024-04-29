01da49aecb5f748d53f7555a87f5d32488b16da7
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-29 15:33:38 +0900

    feat:userId로 찜 목록 찾기 기능 추가

--- backend/src/main/java/com/gracefullyugly/domain/cart/repository/CartRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/cart/repository/CartRepository.java
@@ -1,10 +1,12 @@
 package com.gracefullyugly.domain.cart.repository;
 
 import com.gracefullyugly.domain.cart.entity.Cart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+    Optional<Cart> findCartByUserId(Long userId);
 }
