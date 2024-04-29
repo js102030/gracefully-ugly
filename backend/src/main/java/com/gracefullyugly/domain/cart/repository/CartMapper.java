162feef069ce329c3aae2f17d461a4af50cd7c24
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-29 15:30:18 +0900

    feat:찜 목록 생성 기능 추가

--- backend/src/main/java/com/gracefullyugly/domain/cart/repository/CartMapper.java
+++ backend/src/main/java/com/gracefullyugly/domain/cart/repository/CartMapper.java
@@ -3,11 +3,12 @@ package com.gracefullyugly.domain.cart.repository;
 import com.gracefullyugly.domain.cart.dto.CartListResponse;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface CartMapper {
 
+    Integer createNewCart(Long userId);
+
     List<CartListResponse> selectAllCartItems(Long userId);
-    Integer addCartItem();
-    Integer deleteCartItem(Long itemId);
 }
