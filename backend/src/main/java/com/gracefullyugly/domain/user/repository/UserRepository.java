658af9ffa4a19537a4f68392b2aadcc79b7ea012
SeokhyunRew <darkknightttt@naver.com>
2024-04-29 14:40:35 +0900

    feat : User Repository에 refreshtoken 관련해서 찾는 쿼리 추가

--- backend/src/main/java/com/gracefullyugly/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/user/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.gracefullyugly.domain.user.repository;
 
 import com.gracefullyugly.domain.user.entity.User;
 import java.util.Optional;
+
+import com.gracefullyugly.domain.user.enumtype.SignUpType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String userLoginId);
+
+    Optional<Object> findByRefreshToken(String refreshToken);
 }
