84872fcefce926bbf2ea6ebd8979426ef6391185
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-30 13:32:07 +0900

    feat: 네이티브 쿼리 매핑을 위해 인터페이스로 변경

--- backend/src/main/java/com/gracefullyugly/domain/cart/dto/CartListResponse.java
+++ backend/src/main/java/com/gracefullyugly/domain/cart/dto/CartListResponse.java
@@ -1,16 +1,12 @@
 package com.gracefullyugly.domain.cart.dto;
 
-import com.gracefullyugly.domain.item.entity.Item;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class CartListResponse {
-    private Item cartListItem;
-    private Long itemCount;
+public interface CartListResponse {
+
+    Long getCartItemId();
+    Long getItemCount();
+    Long getItemId();
+    String getName();
+    int getPrice();
+    Long getCategoryId();
 }
