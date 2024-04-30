87bd925c61a37a6062fdb5c29eb7726044632b99
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-04-30 10:44:06 +0900

    fix : Category enum 수정

--- backend/src/main/java/com/gracefullyugly/domain/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gracefullyugly.domain.category.repository;
-
-import com.gracefullyugly.domain.category.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
