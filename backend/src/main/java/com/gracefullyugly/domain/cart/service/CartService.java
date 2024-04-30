51c1b79b024818e0a6de836abf1034a79e03b0a5
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-30 13:32:25 +0900

    feat: ReadOnly로 변경

--- backend/src/main/java/com/gracefullyugly/domain/cart/service/CartService.java
+++ backend/src/main/java/com/gracefullyugly/domain/cart/service/CartService.java
@@ -2,14 +2,14 @@ package com.gracefullyugly.domain.cart.service;
 
 import com.gracefullyugly.domain.cart.dto.CartListResponse;
 import com.gracefullyugly.domain.cart.repository.CartRepository;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class CartService {
 
     private final CartRepository cartRepository;
