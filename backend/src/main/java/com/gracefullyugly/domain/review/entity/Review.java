48ff6db7a2cbe6190a2b9ec8b6211c28d833ed48
js <2kpppk@gmail.com>
2024-05-01 12:56:51 +0900

    feat : 리뷰 전체 기능

--- backend/src/main/java/com/gracefullyugly/domain/review/entity/Review.java
+++ backend/src/main/java/com/gracefullyugly/domain/review/entity/Review.java
@@ -1,11 +1,13 @@
 package com.gracefullyugly.domain.review.entity;
 
 import com.gracefullyugly.common.base.BaseTimeEntity;
+import com.gracefullyugly.domain.review.dto.ReviewDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,7 +27,30 @@ public class Review extends BaseTimeEntity {
 
     private String comments;
 
-    private int starPoint;
+    private float starPoint;
 
     private boolean isDeleted;
+
+    @Builder
+    public Review(Long userId, Long itemId, String comments, float starPoint) {
+        this.userId = userId;
+        this.itemId = itemId;
+        this.comments = comments;
+        this.starPoint = starPoint;
+    }
+
+    public ReviewDto updateReview(ReviewDto reviewRequest) {
+        this.comments = reviewRequest.getComments();
+        this.starPoint = reviewRequest.getStarPoint();
+
+        return ReviewDto.builder()
+                .comments(this.comments)
+                .starPoint(this.starPoint)
+                .build();
+    }
+
+    public void deleteReview() {
+        this.isDeleted = true;
+    }
+
 }
