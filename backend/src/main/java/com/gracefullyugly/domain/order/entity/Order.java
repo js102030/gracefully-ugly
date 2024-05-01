5b58d7c7dbb012dfb1103dd9696f864af235c6ff
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-01 09:23:22 +0900

    feat: Order 엔티티 생성자, 필드 수정 메소드 추가

--- backend/src/main/java/com/gracefullyugly/domain/order/entity/Order.java
+++ backend/src/main/java/com/gracefullyugly/domain/order/entity/Order.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Table(name = "orders")
+@DynamicUpdate
 public class Order extends BaseTimeEntity {
 
     @Id
@@ -26,4 +28,20 @@ public class Order extends BaseTimeEntity {
     private String address;
 
     private String phoneNumber;
+
+    public Order(Long userId, String address, String phoneNumber) {
+        this.userId = userId;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Order updateAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Order updatePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
 }
