69cf25b8ab2353a96f87501d32621c8ecc9e7fe9
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-05-01 10:44:26 +0900

    feat : Item 인기 상품, 종류별 검색 조회 기능 생성

--- backend/src/main/java/com/gracefullyugly/domain/item/repository/ItemRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/item/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.gracefullyugly.domain.item.repository;
 
 import com.gracefullyugly.domain.item.entity.Item;
+import com.gracefullyugly.domain.item.enumtype.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +11,10 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("select i from Item i where i.closedDate >= CURRENT_TIMESTAMP and i.closedDate <= :endTime order by function('rand') ")
     List<Item> findRandomImpendingItems(LocalDateTime endTime);
+
+    @Query("select i, ci.itemCount as salesCount from Item i inner join CartItem ci on i.id = ci.id group by i order by salesCount DESC limit 3")
+    List<Item> findPopularityItems();
+
+    @Query("select i from Item i where i.categoryId = :categoryId order by i.id")
+    List<Item> findCategoryItems(Category categoryId);
 }
