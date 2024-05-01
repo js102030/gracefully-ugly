85710dec49eb0c7eafd7c416e3ad79d175b1c8ef
SeokhyunRew <darkknightttt@naver.com>
2024-05-01 17:42:52 +0900

    feat : UserRepository 에서 반환 값 User > Optional<User> 로 수정
    - 소셜로그인 연동은 아직 x

--- backend/src/main/java/com/gracefullyugly/common/security/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/gracefullyugly/common/security/service/CustomUserDetailsService.java
@@ -23,15 +23,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
 
-        //DB에서 조회
-        User userData = userRepository.findByLoginId(loginId);
+        // DB에서 조회
+        Optional<User> userDataOptional = userRepository.findByLoginId(loginId);
 
-        if (userData!= null) {
+        // Optional에서 User 객체를 가져오거나, 값이 없으면 예외를 던집니다.
+        User userData = userDataOptional.orElseThrow(() -> new UsernameNotFoundException("User not found with loginId: " + loginId));
 
-            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
-            return new CustomUserDetails(userData);
-        }
-
-        return null;
+        // UserDetails에 담아서 return하면 AuthenticationManager가 검증 함
+        return new CustomUserDetails(userData);
     }
 }
--- backend/src/main/java/com/gracefullyugly/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/user/repository/UserRepository.java
@@ -7,7 +7,7 @@ import com.gracefullyugly.domain.user.enumtype.SignUpType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByLoginId(String userLoginId);
+    Optional<User> findByLoginId(String userLoginId);
 
     Optional<User> findByRefreshToken(String refreshToken);
 
