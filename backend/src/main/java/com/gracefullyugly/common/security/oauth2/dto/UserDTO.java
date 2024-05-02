542e81753cc8e9a305eb4e48b233a19af126fdc7
SeokhyunRew <darkknightttt@naver.com>
2024-05-02 17:21:50 +0900

    feat : Oauth2 로그인 필요한 dto 생성

---
+++ backend/src/main/java/com/gracefullyugly/common/security/oauth2/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.gracefullyugly.common.security.oauth2.dto;
+
+import com.gracefullyugly.domain.user.enumtype.Role;
+import com.gracefullyugly.domain.user.enumtype.SignUpType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private Role role;
+    private String username;
+    private Long userId;
+}
