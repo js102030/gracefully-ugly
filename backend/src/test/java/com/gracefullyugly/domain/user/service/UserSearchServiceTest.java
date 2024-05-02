97b1a31b7a3619e7b96921a417d9b9a7b0795955
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-05-02 12:05:34 +0900

    feat : Item 테스트 코드 작성

--- backend/src/test/java/com/gracefullyugly/domain/user/service/UserSearchServiceTest.java
+++ backend/src/test/java/com/gracefullyugly/domain/user/service/UserSearchServiceTest.java
@@ -15,7 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
-class eUserSearchServiceTest {
+class UserSearchServiceTest {
 
     @Autowired
     UserSearchService userSearchService;
