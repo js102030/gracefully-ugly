ad937a0377f62753b90de9cfa0add36bad6d764c
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-02 15:12:31 +0900

    feat: 공동 구매 정보 조회 쿼리 작성

--- backend/src/main/java/com/gracefullyugly/domain/groupbuy/repository/GroupBuyRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/groupbuy/repository/GroupBuyRepository.java
@@ -1,6 +1,9 @@
 package com.gracefullyugly.domain.groupbuy.repository;
 
+import com.gracefullyugly.domain.groupbuy.dto.GroupBuyInfoResponse;
+import com.gracefullyugly.domain.groupbuy.dto.GroupBuySelectDto;
 import com.gracefullyugly.domain.groupbuy.entity.GroupBuy;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,9 +13,29 @@ import org.springframework.stereotype.Repository;
 public interface GroupBuyRepository extends JpaRepository<GroupBuy, Long> {
 
     @Query(value =
+        "SELECT GB.group_buy_id AS groupBuyId, GB.item_id AS itemId, I.name AS itemName, GB.group_buy_status AS groupBuyStatus, GB.end_date AS endDate, COUNT(GBU.group_buy_user_id) AS participantCount "
+      + "FROM group_buy AS GB "
+      + "LEFT OUTER JOIN item AS I ON GB.item_id = I.item_id "
+      + "LEFT OUTER JOIN group_buy_user AS GBU ON GB.group_buy_id = GBU.group_buy_id "
+      + "WHERE GB.group_buy_id = :groupBuyId",
+    nativeQuery = true)
+    Optional<GroupBuyInfoResponse> findGroupBuyById(Long groupBuyId);
+
+    @Query(value =
+        "SELECT GB.group_buy_id AS groupBuyId, GB.item_id AS itemId, GB.group_buy_status AS groupBuyStatus, GB.end_date AS endDate, COUNT(GBU.group_buy_user_id) AS participantCount "
+      + "FROM group_buy AS GB "
+      + "LEFT OUTER JOIN group_buy_user AS GBU ON GB.group_buy_id = GBU.group_buy_id "
+      + "WHERE GB.item_id = :itemId "
+      + "GROUP BY GB.group_buy_id "
+      + "ORDER BY GB.group_buy_id DESC "
+      + "LIMIT 5",
+        nativeQuery = true)
+    List<GroupBuySelectDto> findTop5ByItemIdOrderByIdDesc(Long itemId);
+
+    @Query(value =
         "SELECT * "
-      + "FROM GroupBuy AS GB "
+      + "FROM group_buy AS GB "
       + "WHERE GB.item_id = :itemId AND GB.group_buy_status = '진행중' AND GB.end_date > NOW()",
     nativeQuery = true)
-    Optional<GroupBuy> findGroupBuyByItemId(Long itemId);
+    Optional<GroupBuy> findProgressGroupBuyByItemId(Long itemId);
 }
