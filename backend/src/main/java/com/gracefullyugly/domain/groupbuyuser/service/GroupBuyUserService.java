bf1868e9a14372b19c8c9a7c68b6571125220fbd
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-03 11:11:12 +0900

    feat: 회원의 유효한 공동 구매 참여 정보를 조회 Service 메소드 작성

--- backend/src/main/java/com/gracefullyugly/domain/groupbuyuser/service/GroupBuyUserService.java
+++ backend/src/main/java/com/gracefullyugly/domain/groupbuyuser/service/GroupBuyUserService.java
@@ -4,6 +4,7 @@ import com.gracefullyugly.common.exception.custom.NotFoundException;
 import com.gracefullyugly.domain.groupbuy.entity.GroupBuy;
 import com.gracefullyugly.domain.groupbuy.enumtype.GroupBuyStatus;
 import com.gracefullyugly.domain.groupbuy.repository.GroupBuyRepository;
+import com.gracefullyugly.domain.groupbuyuser.dto.GroupBuyUserFindResponse;
 import com.gracefullyugly.domain.groupbuyuser.entity.GroupBuyUser;
 import com.gracefullyugly.domain.groupbuyuser.repository.GroupBuyUserRepository;
 import com.gracefullyugly.domain.item.entity.Item;
@@ -27,23 +28,34 @@ public class GroupBuyUserService {
         orderItemList.forEach(orderItem -> {
             Long groupId = getGroupBuyId(orderItem.getItemId());
 
-            groupBuyUserRepository.save(new GroupBuyUser(groupId, userId, LocalDateTime.now(), orderItem.getQuantity()));
+            groupBuyUserRepository.save(
+                    new GroupBuyUser(groupId, userId, LocalDateTime.now(), orderItem.getQuantity()));
         });
     }
 
+    public GroupBuyUserFindResponse getGroupBuyUser(Long userId, Long itemId) {
+        GroupBuyUser groupBuyUser = groupBuyUserRepository.findByUserIdAndItemId(userId, itemId)
+                .orElseThrow(() -> new NotFoundException("공동 구매 참여 정보가 없습니다."));
+
+        return GroupBuyUserFindResponse.builder()
+                .joinDate(groupBuyUser.getJoinDate())
+                .quantity(groupBuyUser.getQuantity())
+                .build();
+    }
+
     /**
-     * 해당 상품의 현재 진행중인 공동 구매를 조사해 그 공동 구매의 ID를 반환하는 메소드 입니다.
-     * 만약, 진행중인 공동 구매가 없다면 공동 구매를 생성한 뒤 그 ID를 반환합니다.
+     * 해당 상품의 현재 진행중인 공동 구매를 조사해 그 공동 구매의 ID를 반환하는 메소드 입니다. 만약, 진행중인 공동 구매가 없다면 공동 구매를 생성한 뒤 그 ID를 반환합니다.
      */
     private Long getGroupBuyId(Long itemId) {
         Optional<GroupBuy> groupBuyOptional = groupBuyRepository.findProgressGroupBuyByItemId(itemId);
 
         if (groupBuyOptional.isEmpty()) {
-            Item result = itemRepository.findValidItemById(itemId).orElseThrow(() -> new NotFoundException("유효한 상품이 아닙니다. (item id: " + itemId + ")"));
+            Item result = itemRepository.findValidItemById(itemId)
+                    .orElseThrow(() -> new NotFoundException("유효한 상품이 아닙니다. (item id: " + itemId + ")"));
 
-            return groupBuyRepository.save(new GroupBuy(itemId, GroupBuyStatus.IN_PROGRESS, result.getClosedDate())).getId();
-        }
-        else {
+            return groupBuyRepository.save(new GroupBuy(itemId, GroupBuyStatus.IN_PROGRESS, result.getClosedDate()))
+                    .getId();
+        } else {
             return groupBuyOptional.get().getId();
         }
     }
