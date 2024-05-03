2a16d6b5a0b00752941de919a291c173e6ca454b
SeokhyunRew <darkknightttt@naver.com>
2024-05-03 20:52:49 +0900

    feat : Null 인경우 예외처리 표시

--- backend/src/main/java/com/gracefullyugly/common/security/oauth2/dto/KakaoResponse.java
+++ backend/src/main/java/com/gracefullyugly/common/security/oauth2/dto/KakaoResponse.java
@@ -2,13 +2,18 @@ package com.gracefullyugly.common.security.oauth2.dto;
 
 import java.util.Map;
 
-public class KakaoResponse implements OAuth2Response{
+public class KakaoResponse implements OAuth2Response {
 
     private final Map<String, Object> attribute;
 
     public KakaoResponse(Map<String, Object> attribute) {
 
-        this.attribute = (Map<String, Object>) attribute.get("response");
+        if (attribute != null) {
+            this.attribute = attribute;
+        } else {
+            System.out.println("attribute에 null 값들어옴");
+            this.attribute = null; // 또는 적절한 처리를 해줍니다.
+        }
     }
 
     @Override
@@ -20,6 +25,14 @@ public class KakaoResponse implements OAuth2Response{
     @Override
     public String getProviderId() {
 
-        return attribute.get("id").toString();
+        if (attribute != null) {
+            Object id = attribute.get("id");
+            if (id != null) {
+                return id.toString();
+            }
+            System.out.println("attribute 의 id가 null이야");
+        }
+        System.out.println("attribute 가 null 이야");
+        return "attribute is null"; // 또는 적절한 처리를 해줍니다.
     }
 }
