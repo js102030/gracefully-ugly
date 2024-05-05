cc57b3a9e5884688d25e1cce59d38488c219c8f3
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-05 23:27:53 +0900

    feat: 공동 구매 완료 조건에 따라 공동 구매 상태 변경 쿼리 작성

--- backend/src/main/java/com/gracefullyugly/domain/groupbuy/repository/GroupBuyRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/groupbuy/repository/GroupBuyRepository.java
@@ -37,6 +37,19 @@ public interface GroupBuyRepository extends JpaRepository<GroupBuy, Long> {
     Optional<GroupBuy> findProgressGroupBuyByItemId(Long itemId);
 
     @Modifying
+    @Query(value = "UPDATE group_buy AS GB "
+            + "SET GB.group_buy_status = 'COMPLETED' "
+            + "WHERE GB.group_buy_id IN ("
+            + "SELECT GB.group_buy_id "
+            + "FROM GB "
+            + "LEFT OUTER JOIN item AS I ON GB.itemId = I.item_id "
+            + "LEFT OUTER JOIN group_buy_user AS GBU ON GB.group_buy_id = GBU.group_buy_id "
+            + "WHERE GB.group_buy_id = :groupId AND "
+            + "I.price * ((SELECT SUM(GBU.quantity) FROM GBU WHERE GBU.group_buy_id = GB.group_buy_id) / I.min_unit_weight) >= I.min_group_buy_weight)",
+            nativeQuery = true)
+    void updateGroupBuyStatusByGroupId(Long groupId);
+
+    @Modifying
     @Query("UPDATE GroupBuy AS GB "
             + "SET GB.groupBuyStatus = com.gracefullyugly.domain.groupbuy.enumtype.GroupBuyStatus.CANCELLED "
             + "WHERE GB.groupBuyStatus = com.gracefullyugly.domain.groupbuy.enumtype.GroupBuyStatus.IN_PROGRESS AND GB.endDate <= CURRENT_TIMESTAMP")
