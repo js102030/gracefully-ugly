05f9331893e2abb13be55c53a956c949528854e8
js <2kpppk@gmail.com>
2024-05-06 21:05:04 +0900

    feat : admin, admin-report html 추가

--- backend/src/main/java/com/gracefullyugly/common/controller/CommonController.java
+++ backend/src/main/java/com/gracefullyugly/common/controller/CommonController.java
@@ -40,4 +40,34 @@ public class CommonController {
     public String sellerDetails() {
         return "sellerDetails";
     }
+
+    @GetMapping("/check-order")
+    public String checkOrder() {
+        return "check-order";
+    }
+
+    @GetMapping("/create-order")
+    public String createOrder() {
+        return "create-order";
+    }
+
+    @GetMapping("/create-review")
+    public String createReview() {
+        return "create-review";
+    }
+
+    @GetMapping("/group-buying")
+    public String groupBuying() {
+        return "group-buying";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/admin-report")
+    public String adminReport() {
+        return "admin-report";
+    }
 }
--- backend/src/main/java/com/gracefullyugly/common/security/config/SecurityConfig.java
+++ backend/src/main/java/com/gracefullyugly/common/security/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/api/users",
                                 "/api/users/{userId}/registration", "/**",
                                 "/logout", "/reissue",
-                                "/my-page","/join","/join2","/main")
+                                "/my-page", "/join", "/join2", "/main")
                         .permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
@@ -96,7 +96,8 @@ public class SecurityConfig {
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, userRepository),
+                .addFilterAt(
+                        new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, userRepository),
                         UsernamePasswordAuthenticationFilter.class);
 
         http
