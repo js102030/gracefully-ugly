7f2a251d7519b18ab0a8ab3871e7a4a6ed010475
js <2kpppk@gmail.com>
2024-05-08 18:48:15 +0900

    feat : 로그 엔티티 수정

--- backend/src/main/java/com/gracefullyugly/domain/log/entity/Log.java
+++ backend/src/main/java/com/gracefullyugly/domain/log/entity/Log.java
@@ -3,6 +3,8 @@ package com.gracefullyugly.domain.log.entity;
 import com.gracefullyugly.domain.log.enumtype.LogLevel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,6 +27,7 @@ public class Log {
 
     private LocalDateTime logTime;
 
+    @Enumerated(EnumType.STRING)
     private LogLevel logLevel;
 
     private String methodSignature;
