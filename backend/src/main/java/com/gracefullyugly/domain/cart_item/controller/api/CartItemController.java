013d474f1cb5f1c4e034e67988ca24ca4d68ac3f
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-09 17:22:35 +0900

    feat: @AuthenticationPrincipal 적용

--- backend/src/main/java/com/gracefullyugly/domain/cart_item/controller/api/CartItemController.java
+++ backend/src/main/java/com/gracefullyugly/domain/cart_item/controller/api/CartItemController.java
@@ -4,8 +4,10 @@ import com.gracefullyugly.domain.cart_item.dto.AddCartItemRequest;
 import com.gracefullyugly.domain.cart_item.dto.CartItemResponse;
 import com.gracefullyugly.domain.cart_item.service.CartItemService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +22,17 @@ public class CartItemController {
 
     private final CartItemService cartItemService;
 
-    @PostMapping("/cart/{userId}/{itemId}")
-    public ResponseEntity<CartItemResponse> addCartItem(@PathVariable(name = "userId") Long userId, @PathVariable(name = "itemId") Long itemId, @Valid @RequestBody AddCartItemRequest request) {
+    @PostMapping("/cart/{itemId}")
+    public ResponseEntity<CartItemResponse> addCartItem(
+            @Valid @NotNull @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable(name = "itemId") Long itemId, @Valid @RequestBody AddCartItemRequest request) {
         return ResponseEntity.ok(cartItemService.addCartItem(userId, itemId, request));
     }
 
-    @DeleteMapping("/cart/{userId}/{itemId}")
-    public ResponseEntity<CartItemResponse> deleteCartItem(@PathVariable(name = "userId") Long userId, @PathVariable(name = "itemId") Long itemId) {
+    @DeleteMapping("/cart/{itemId}")
+    public ResponseEntity<CartItemResponse> deleteCartItem(
+            @Valid @NotNull @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable(name = "itemId") Long itemId) {
         return ResponseEntity.ok(cartItemService.deleteCartItem(userId, itemId));
     }
 }
