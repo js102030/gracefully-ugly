5867404df23c16e25ed3e02869243fa6eae46e2a
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-09 20:24:08 +0900

    feat: 각 버튼 이벤트 핸들러 등록

--- backend/src/main/resources/static/js/cart-list.js
+++ backend/src/main/resources/static/js/cart-list.js
@@ -21,3 +21,27 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+function moveToGroupBuying(itemId) {
+    window.location.href = ('http://localhost:8080/group-buying?itemId=' + itemId);
+}
+
+function deleteCartItem(cartItemId) {
+    $.ajax({
+        url: "http://localhost:8080/api/cart/" + cartItemId,
+        method: 'delete',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            alert("해당 상품이 찜 목록에서 삭제되었습니다.");
+            window.location.reload();
+        },
+        error: function (data, status, error) {
+            alert('삭제 도중 문제가 발생했습니다.\n[status: ' + data.status +']\n[error: ' + data.responseText + ']');
+        }
+    })
+}
+
+function orderCartItem(itemId) {
+    window.location.href = ('http://localhost:8080/orders/item/' + itemId);
+}
