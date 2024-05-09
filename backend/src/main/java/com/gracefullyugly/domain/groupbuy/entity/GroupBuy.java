cdfda8af8f3c9bf01eb2acceb7f7c25e4ee0aa82
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-09 11:31:02 +0900

    fix: ENUM 부분 Enumerated 추가

--- backend/src/main/java/com/gracefullyugly/domain/groupbuy/entity/GroupBuy.java
+++ backend/src/main/java/com/gracefullyugly/domain/groupbuy/entity/GroupBuy.java
@@ -4,6 +4,8 @@ import com.gracefullyugly.common.base.BaseTimeEntity;
 import com.gracefullyugly.domain.groupbuy.enumtype.GroupBuyStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,6 +25,7 @@ public class GroupBuy extends BaseTimeEntity {
 
     private Long itemId;
 
+    @Enumerated(EnumType.STRING)
     private GroupBuyStatus groupBuyStatus;
 
     private LocalDateTime endDate;
