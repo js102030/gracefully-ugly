7e3558ebbd905ae0bd83ea33d3a02a19ae7eafe7
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-10 17:34:32 +0900

    feat: getPaymentList 뷰 컨트롤러 추가

--- backend/src/main/java/com/gracefullyugly/domain/payment/controller/PaymentViewController.java
+++ backend/src/main/java/com/gracefullyugly/domain/payment/controller/PaymentViewController.java
@@ -1,8 +1,13 @@
 package com.gracefullyugly.domain.payment.controller;
 
 import com.gracefullyugly.domain.payment.dto.KakaoPayApproveResponse;
+import com.gracefullyugly.domain.payment.dto.PaymentSearchListResponse;
+import com.gracefullyugly.domain.payment.service.PaymentSearchService;
 import com.gracefullyugly.domain.payment.service.PaymentService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +19,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PaymentViewController {
 
     private PaymentService paymentService;
+    private PaymentSearchService paymentSearchService;
+
+    @GetMapping("/payment")
+    public String getPaymentList(
+            @Valid @NotNull @AuthenticationPrincipal(expression = "userId") Long userId,
+            Model model) {
+        PaymentSearchListResponse response = paymentSearchService.getPaymentList(userId);
+        model.addAttribute("PaymentSearchListResponse", response);
+
+        return "/purchase_history";
+    }
 
     @GetMapping("/payment/kakaopay/success/{userId}/{orderId}")
     public String approveKakaoPay(@PathVariable("userId") Long userId,
@@ -26,7 +42,6 @@ public class PaymentViewController {
         return "/complete-payment";
     }
 
-    // ToDo: 카카오 페이 결제 취소 시 보여줄 페이지 제작 필요
     @GetMapping("/payment/cancel")
     public String cancelKakaoPay() {
 
