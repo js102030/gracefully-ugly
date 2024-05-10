964fe48d4e2123b3ddfa3b45a5a82dc64172eaf4
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-05-10 17:31:03 +0900

    fix: 쿼리 변경

--- backend/src/main/java/com/gracefullyugly/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/gracefullyugly/domain/payment/repository/PaymentRepository.java
@@ -11,23 +11,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    @Query("SELECT new com.gracefullyugly.domain.payment.dto.PaymentSearchOrderDto(O.id, O.address, O.phoneNumber) AS order, "
-            + "new com.gracefullyugly.domain.payment.dto.PaymentSearchItemDto(I.id, I.categoryId, I.name, I.price, OI.quantity) AS item, "
-            + "new com.gracefullyugly.domain.payment.dto.PaymentSearchPaymentDto(P.id, P.tid, P.totalPrice, P.createdDate, P.isPaid, P.isRefunded) AS payment "
+    @Query("SELECT new com.gracefullyugly.domain.payment.dto.PaymentSearchDTO(O.id, O.address, O.phoneNumber, P.id, P.totalPrice, P.lastModifiedDate, P.isPaid, P.isRefunded) "
             + "FROM Payment AS P "
             + "LEFT OUTER JOIN Order AS O ON P.orderId = O.id "
-            + "LEFT OUTER JOIN OrderItem AS OI ON O.id = OI.ordersId "
-            + "LEFT OUTER JOIN Item AS I ON OI.itemId = I.id "
-            + "WHERE O.userId = :userId")
+            + "WHERE O.userId = :userId "
+            + "ORDER BY P.lastModifiedDate DESC "
+            + "LIMIT 30")
     List<PaymentSearchDTO> findPaymentsByUserId(Long userId);
 
-    @Query("SELECT new com.gracefullyugly.domain.payment.dto.PaymentSearchOrderDto(O.id, O.address, O.phoneNumber) AS order, "
-            + "new com.gracefullyugly.domain.payment.dto.PaymentSearchItemDto(I.id, I.categoryId, I.name, I.price, OI.quantity) AS item, "
-            + "new com.gracefullyugly.domain.payment.dto.PaymentSearchPaymentDto(P.id, P.tid, P.totalPrice, P.createdDate, P.isPaid, P.isRefunded) AS payment "
+    @Query("SELECT new com.gracefullyugly.domain.payment.dto.PaymentSearchDTO(O.id, O.address, O.phoneNumber, P.id, P.totalPrice, P.lastModifiedDate, P.isPaid, P.isRefunded) "
             + "FROM Payment AS P "
             + "LEFT OUTER JOIN Order AS O ON P.orderId = O.id "
-            + "LEFT OUTER JOIN OrderItem AS OI ON O.id = OI.ordersId "
-            + "LEFT OUTER JOIN Item AS I ON OI.itemId = I.id "
             + "WHERE O.userId = :userId AND P.orderId = :orderId")
     Optional<PaymentSearchDTO> findPaymentByUserIdAndOrderId(Long userId, Long orderId);
 
