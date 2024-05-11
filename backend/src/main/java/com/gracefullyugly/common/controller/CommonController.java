16551a107bcec725ad0a7a9cd545914aba8cc9a1
SeokhyunRew <darkknightttt@naver.com>
2024-05-11 19:01:14 +0900

    feat : security controller로 따로관리

--- backend/src/main/java/com/gracefullyugly/common/controller/CommonController.java
+++ backend/src/main/java/com/gracefullyugly/common/controller/CommonController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -103,11 +102,6 @@ public class CommonController {
     @GetMapping("/log")
     public String log() {
         return "login";
-    }
-
-    @PostMapping("/login")
-    public String login() {
-        return "main";
     }
 
     @GetMapping("/cart-list")
