0bfbf85d8d3ad74cb5f3fd677ecb3d1ef4ae60bf
SeokhyunRew <darkknightttt@naver.com>
2024-05-15 13:52:25 +0900

    feat : 뒤로가기 버튼 기능 추가

--- src/main/resources/static/js/productAsk.js
+++ src/main/resources/static/js/productAsk.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const askAddButton = document.querySelector('.askAdd');
     const item = document.getElementById('itemId');
 
-        askAddButton.addEventListener('click', function () {
+    askAddButton.addEventListener('click', function () {
         const askContent = askTextarea.value.trim();
-        const itemId =item.value;
+        const itemId = item.value;
 
         // 만약 입력된 내용이 없다면 처리 중지
         if (askContent === '') {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         // Fetch를 사용하여 API 호출
-        fetch('/api/questions/'+itemId, {
+        fetch('/api/questions/' + itemId, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -63,7 +63,7 @@ function addAnswer(event) {
         answer: answerContent
     };
 
-    fetch(`/api/answers/`+qnaId, {
+    fetch(`/api/answers/` + qnaId, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -71,10 +71,10 @@ function addAnswer(event) {
         body: JSON.stringify(requestData)
     })
         .then(response => {
-            if(response.ok){
+            if (response.ok) {
                 alert("답변 등록에 성공하셨습니다.");
                 window.location.reload();
-            } else{
+            } else {
                 throw new Error('Failed to submit answer');
             }
         })
@@ -84,8 +84,8 @@ function addAnswer(event) {
         });
 }
 
-//----------------------------답변하기누르면 해당 칸에만 답변하는 상자 나오게 하는게 어려워서 일단 킵----------------
-function showAddAnswer(event){
+//----------------------------답변하기누르면 해당 칸에만 답변하는 상자 나오게 함----------------
+function showAddAnswer(event) {
     const container = event.target.closest('.oneRoute');
     let qnaId = container.querySelector('#qnaId').value;
     console.log(container);
@@ -102,3 +102,8 @@ function showAddAnswer(event){
         console.error(`Answer input with ID 'answer_input_${qnaId}' not found.`);
     }
 }
+
+// --------------------------------뒤로 가기 버튼--------------------------------------
+document.querySelector('.back').addEventListener('click', function() {
+    window.history.back();
+});
