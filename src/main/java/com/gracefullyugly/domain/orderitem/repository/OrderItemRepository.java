f25f4fc97be7f6a19e7b9aeea987e8df3fa686ff
js <2kpppk@gmail.com>
2024-05-18 21:14:29 +0900

    refactor : 변수명 변경

--- src/main/java/com/gracefullyugly/domain/orderitem/repository/OrderItemRepository.java
+++ src/main/java/com/gracefullyugly/domain/orderitem/repository/OrderItemRepository.java
@@ -17,10 +17,10 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
             "SELECT new com.gracefullyugly.domain.orderitem.dto.OrderItemInfoResponse(I.id, I.categoryId, I.name, I.price, OI.quantity) AS item "
                     + "FROM OrderItem AS OI "
                     + "LEFT OUTER JOIN Item AS I ON OI.itemId = I.id "
-                    + "WHERE OI.ordersId = :orderId ")
+                    + "WHERE OI.orderId = :orderId ")
     List<OrderItemInfoResponse> getOrderItemListByOrderId(Long orderId);
 
-    List<OrderItem> findAllByOrdersId(Long orderId);
+    List<OrderItem> findAllByOrderId(Long orderId);
 
     @Query(value =
             "SELECT I.name AS itemName, " +
@@ -30,14 +30,14 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
                     "O.address AS address, " +
                     "O.created_date AS orderCreatedDate " +
                     "FROM item I " +
-                    "LEFT JOIN orders_item OI ON I.item_id = OI.item_id " +
-                    "LEFT JOIN orders O ON OI.orders_id = O.order_id " +
+                    "LEFT JOIN order_item OI ON I.item_id = OI.item_id " +
+                    "LEFT JOIN orders O ON OI.order_id = O.order_id " +
                     "WHERE O.order_id = :orderId", nativeQuery = true)
     OrderItemInfo findOrderItemInfo(@Param("orderId") Long orderId);
 
     @Query("SELECT new com.gracefullyugly.domain.orderitem.dto.ItemOrderDetails(i, oi.quantity) " +
             "FROM OrderItem oi " +
             "JOIN Item i ON oi.itemId = i.id " +
-            "WHERE oi.ordersId = :orderId")
+            "WHERE oi.orderId = :orderId")
     List<ItemOrderDetails> findItemsAndQuantityByOrderId(@Param("orderId") Long orderId);
 }
