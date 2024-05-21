9908b1ff8f2fb60f26d76ea81e794bea8956e69c
js <2kpppk@gmail.com>
2024-05-21 22:34:32 +0900

    refactor : 캔슬된 공동구매 환불 로직 추가.

--- src/main/java/com/gracefullyugly/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/gracefullyugly/domain/payment/repository/PaymentRepository.java
@@ -5,6 +5,7 @@ import com.gracefullyugly.domain.payment.entity.Payment;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -36,5 +37,20 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
                     + "WHERE O.user_id = :userId AND P.is_paid = true AND P.is_refunded = false ",
             nativeQuery = true)
     Integer getBuyCountByUserId(Long userId);
+
+    @Modifying
+    @Query(value = "UPDATE payment p " +
+            "SET p.is_refunded = true " +
+            "WHERE p.id IN (" +
+            "    SELECT p.id " +
+            "    FROM payment p " +
+            "    INNER JOIN group_buy_user gbu ON p.order_id = gbu.order_id " +
+            "    INNER JOIN group_buy gb ON gbu.group_buy_id = gb.group_buy_id " +
+            "    WHERE gb.groupBuyStatus = 'CANCELLED' " +
+            "    AND p.is_refunded = false" +
+            ")",
+            nativeQuery = true)
+    int updatePaymentsToRefundedForCancelledGroupBuys();
+
 
 }
